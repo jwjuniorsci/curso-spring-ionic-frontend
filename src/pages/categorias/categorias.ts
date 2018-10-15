21e06bf8487f2da7a03833fc98c2666af09ebfae
JW Junior <jwjuniorsi@gmail.com>
2018-10-15 13:29:24 -0300

    Mostrando Categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
-
+import { CategoriaDTO } from './../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 
 @IonicPage()
@@ -10,6 +11,9 @@ import { CategoriaService } from '../../services/domain/categoria.service';
   templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
+
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+  items: CategoriaDTO[];
 
   constructor(
     public navCtrl: NavController,
@@ -21,7 +25,7 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll()
       .subscribe(response => {
-        console.log(response);
+        this.items = response;
       },
     error => {
       console.log(error);
